0adff9179578951c93d32e10f3297de5bd3f3d8f
basystka <basystka@gmail.com>
2018-09-13 20:29:43 +0300

    2

--- src/getFlags/index.js
+++ src/getFlags/index.js
@@ -6,10 +6,13 @@
  * getFlags(arr); // [1, 2, 3, 4, 5, 6]
  */
 const getFlags = arr => {
- for(i=0; i < arr.length; i++){
- 	getFlags.push(arr[i]+arr2[i]);
+	let newArr = [];
+ for(let i=0; i < arr.length; i++){
+ 	for(let j = 0; j < arr[i].flags.length; j++)
+ 	newArr.push(arr[i].flags[j]); 
  }
- return getFlags;
+ return newArr;
 };
 
 export default getFlags;
+ 
